3251c36143c4d2504bd03e622baacd90912f5829
Calebjazz <kalebumuladi@gmail.com>
2026-06-15 19:32:43 +0300

    restructured project folders

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+$Host = "localhost";
+$username = "root";
+$password = "";
+$database = "financedb";
+
+//connection to database
+$conn = mysqli_connect( "localhost", "root", "", "financedb" );
+
+// Checking connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// if connected
+echo "Connected Successfully";
+
+?>
